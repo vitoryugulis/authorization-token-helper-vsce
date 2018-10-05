689828ae144ab62e6863cd4dc2b6fcf6352b0577
vitoryugulis <vitor.borges@cedrotech.com>
2018-10-05 09:51:16 -0300

    Refatoração + atualização do readme + tratamento de exceção

--- README.md
+++ README.md
@@ -9,22 +9,34 @@ Comandos:
 - requireqatoken -> Envia a requisição para api de autenticação em QA, recupera o token e escreve o cookie no app.module.ts
 - requiredevtoken -> Envia a requisição para api de autenticação em DEV, recupera o token e escreve o cookie no app.module.ts
 
+Como chamar o comando? Digite:
+- CTRL + SHIFT + P
+- escreva o comando requireqatoken ou requiredevtoken
+- ENTER
+
+Depois que o comando estiver registrado, só precisa apertar CTRL + SHIFT + P e depois ENTER
 ## Requirements
 
-Preencher o arquivo "/src/launchsettings.json" com os atributos necessários.
+Preencher o arquivo "/out/launchsettings.json" dentro da pasta da extensão, com os atributos necessários.
 - qa_endpoint -> preencher com o endpoint de QA (omitido na extensão por razões de segurança)
 - dev_endpoint -> preencher com o endpoint de DEV (omitido na extensão por razões de segurança)
 - login -> preencher com o endpoint de QA (omitido na extensão por razões de segurança)
 - password -> preencher com o endpoint de QA (omitido na extensão por razões de segurança)
 
+Necessário estar utilizando a versão 1.19.1 ou superior do VS CODE.
+
 ## Known Issues
 
 - O código está longe de ser perfeito ou bonito. Fiz nas horas vagas da maneira mais rápida possível pois não tinha muito tempo livre, e não aguentava mais ter que abrir o postman e escrever o bendito cookie.
 - A extensão não pega o "path" do app.module.ts se não houver um arquivo aberto, e o arquivo aberto precisa estar localizado dentro da pasta "/src/app".
+- Se seu app.module.ts não tiver a seguinte linha de código "if (!cookieService.check", não vai funcionar a extensão.
+- Não há tratamento de exceção caso o usuário não configure o launchsettings.json corretamente.
 
 ## Release Notes
 
-Primeira versão... funcionalidades descritas acima.
+- Refatoração do  código.
+- Tratamento de exceções
+- Atualização do readme.md
 
-### 1.0.0
-- Release version.
+### 0.0.5
+- Versão experimental. Sugestões e críticas são bem vindas.
--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import { window, commands, ExtensionContext, TextDocument } from 'vscode';
 import * as request from 'request';
-import * as settings from '../src/launchsettings.json';
+import * as settings from '../out/launchsettings.json';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
@@ -27,93 +27,69 @@ export function deactivate() {
 
 class RequestService {
 
-    public getAuthorizationToken(isDev: boolean) {
-        var self = this;
+     getAuthorizationToken(isDev: boolean) {
         let editor = window.activeTextEditor;
         if(!editor){
             window.showErrorMessage("ERRO: Para funcionar a extensão, abra qualquer documento dentro da pasta src/app/");
             return;
         }
         let doc = editor.document;
-
-        var showErrorMessage = function (response: any) {
-            window.showInformationMessage(response.error);
-            window.showInformationMessage(response.error_description);
-        }
-
-        var showSuccessMessage = function (response: any){
-            var requestReponse = "expires_in: " + Math.round(Number(response.expires_in) / 60) + " minutes";
-            window.showInformationMessage(requestReponse);
-            window.showInformationMessage(response.access_token);
-        }
-
-        var writeAuthorizationCookie = function (response: any){
-            var fs = require("fs");
-            var index = doc.fileName.indexOf("\app");
-            if(index == -1){
-                window.showErrorMessage("ERRO: Para funcionar a extensão, abra qualquer documento dentro da pasta src/app/");
-                return;
-            }
-            var appModulePath = doc.fileName.slice(0, index) + "app\\app.module.ts";
-            var content = fs.readFileSync(appModulePath, 'utf8')
-            var newAuth = self.getNewAuthorization(content, response.access_token);
-            fs.writeFile(appModulePath, newAuth, function () { });
-            showSuccessMessage(response);
-        }
-
-        var definePostParametersForQa = function () {
-            return {
-                method: "POST",
-                uri: settings.qa_endpoint,
-                body: JSON.stringify({
-                    "user_name": settings.login,
-                    "password": settings.password
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        }
-
-        var definePostParametersForDev = function () {
-            return {
-                method: "POST",
-                uri: settings.dev_endpoint,
-                body: JSON.stringify({
-                    "user_name": settings.login,
-                    "password": settings.password
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        }
-
-        var showPostFeedback = function (body: any) {
-            let response = JSON.parse(body);
-            if (response.error) {
-                showErrorMessage(response);
-            }
-            else {
-                writeAuthorizationCookie(response);
-            }
-        }
-
-            if(isDev){
-                window.showInformationMessage("Requisição DEV");
-                request.post(definePostParametersForDev(), function (err, httpResponse, body) {
-                    showPostFeedback(body);
-                });
-            }
-            else{
-                window.showInformationMessage("Requisição QA");
-                request.post(definePostParametersForQa(), function (err, httpResponse, body) {
-                    showPostFeedback(body);
-                });
-            }
+        this.start(isDev, doc);
     }
 
-    public getNewAuthorization(docContent: string, accessToken: string) {
+    start(isDev: boolean, doc: TextDocument){
+        let self = this;
+        if(isDev){
+            window.showInformationMessage("Requisição DEV");
+            request.post(this.definePostParameters(isDev), function (err, httpResponse, body) {
+                self.showPostFeedback(body, doc);
+            });
+        }
+        else{
+            window.showInformationMessage("Requisição QA");
+            request.post(this.definePostParameters(isDev), function (err, httpResponse, body) {
+                self.showPostFeedback(body, doc);
+            });
+        }
+    }
+    
+    showPostFeedback(body: any, doc: TextDocument) {
+        let response = JSON.parse(body);
+        if (response.error) {
+            this.showErrorMessage(response);
+        }
+        else {
+            this.writeAuthorizationCookie(response, doc);
+        }
+    }
+
+    showErrorMessage(response: any) {
+        window.showInformationMessage(response.error);
+        window.showInformationMessage(response.error_description);
+    }
+
+    writeAuthorizationCookie(response: any, doc: TextDocument){
+        let self = this;
+        var fs = require("fs");
+        var index = doc.fileName.indexOf("\app");
+        if(index == -1){
+            window.showErrorMessage("ERRO: Para funcionar a extensão, abra qualquer documento dentro da pasta src/app/");
+            return;
+        }
+        var appModulePath = doc.fileName.slice(0, index) + "app\\app.module.ts";
+        var content = fs.readFileSync(appModulePath, 'utf8');
+        var newAuth = self.getNewAuthorization(content, response.access_token);
+        fs.writeFile(appModulePath, newAuth, function () { });
+        self.showSuccessMessage(response);
+    }
+
+    showSuccessMessage(response: any){
+        var requestReponse = "expires_in: " + Math.round(Number(response.expires_in) / 60) + " minutes";
+        window.showInformationMessage(requestReponse);
+        window.showInformationMessage(response.access_token);
+    }
+
+     getNewAuthorization(docContent: string, accessToken: string) {
         var hasPayload = true;
         var payload = "";
         var newAuth = "";
@@ -144,7 +120,21 @@ class RequestService {
         return startIndex != -1 && endIndex != -1;
     }
 
-    public generateNewAuthorization(textInDocument: string, parsedPayload: JSON | string, startIndex: number, endIndex: number, hasPayload: boolean) {
+    definePostParameters(isDev: boolean) {
+        return {
+            method: "POST",
+            uri: isDev? settings.dev_endpoint: settings.qa_endpoint,
+            body: JSON.stringify({
+                "user_name": settings.login,
+                "password": settings.password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    }
+
+    generateNewAuthorization(textInDocument: string, parsedPayload: JSON | string, startIndex: number, endIndex: number, hasPayload: boolean) {
         var fisrtPortionOfText = textInDocument.slice(0, startIndex);
         var secondPortionOfText = textInDocument.slice(endIndex, textInDocument.length);
         var newTextToWrite = "";
@@ -158,14 +148,14 @@ class RequestService {
         return newTextToWrite;
     }
 
-    public getJsonObj(stringJson: string) {
+    getJsonObj(stringJson: string) {
         stringJson = stringJson.replace("const obj = {", "{").replace(";", "");
         var teste = stringJson.replace(/'/g, '"');
         var parsedJsonString = JSON.parse(teste);
         return parsedJsonString;
     }
 
-    public getCookieTemplate() {
+    getCookieTemplate() {
         return `
         const obj = {"acess_token":""};
         const objStr = JSON.stringify(obj);
